c45ed2b38f3aa37145264e9b18fb4a73b3642810
lsy3709 <lsy3709@naver.com>
2026-03-04 10:40:19 +0900

    2교시, 서비스 , DTO 변환 작업 준비중, 3, BoardService, BoardServiceImpl, 단위테스트 확인

---
+++ src/test/java/com/busanit501/springboot0226/service/BoardServiceTests.java
@@ -0,0 +1,28 @@
+package com.busanit501.springboot0226.service;
+
+import com.busanit501.springboot0226.dto.BoardDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class BoardServiceTests {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testRegister() {
+        BoardDTO boardDTO = BoardDTO.builder()
+                .title("오늘 점심 뭐 먹죠?")
+                .content("오늘 점심 뭐 먹죠?")
+                .writer("이상용")
+                .build();
+
+        Long bno = boardService.register(boardDTO);
+       log.info("등록된 bno 확인 : " + bno);
+    }
+
+}
